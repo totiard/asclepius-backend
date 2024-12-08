01495a002d103943cd6c863b069ca50c60f2caac
totiard <165372513+totiard@users.noreply.github.com>
2024-12-09 00:17:19 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/predict-bucket-11/model.json'); //url untuk model ML kalian
+    return tf.loadGraphModel('https://storage.googleapis.com/predict-bucket-11/model.json'); //ganti dengan url untuk model ML kalian
 }
 module.exports = loadModel;
